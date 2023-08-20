c3cda8c3125694ef81390112ee3dd383e1f2b059
maheshnnaik <maheshnnaik07@gmail.com>
2023-08-20 15:49:15 +0530

    remove unwanted code

--- src/component/AddTransaction.tsx
+++ src/component/AddTransaction.tsx
@@ -16,7 +16,7 @@ const modalStyle: SxProps<Theme> = {
 	display: "flex",
 	justifyContent: "center",
 	alignItems: "center",
-	left: "30vw"
+	left: "30vw",
 };
 export const AddTransaction = (props: AddTransactionProps) => {
 	const [category, setCategory] = useState<string>("");
@@ -40,74 +40,72 @@ export const AddTransaction = (props: AddTransactionProps) => {
 	const handleSubmit = () => {};
 
 	return (
-		<div className="add-transaction">
-			<Modal
-				open={props.open}
-				onClose={() => {
-					props.setShowAddExpenseForm(false);
+		<Modal
+			open={props.open}
+			onClose={() => {
+				props.setShowAddExpenseForm(false);
+			}}
+			sx={modalStyle}
+		>
+			<Box
+				component="form"
+				sx={{
+					"& .MuiTextField-root": { m: 3, width: "25ch" },
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					bgcolor: (theme) =>
+						theme.palette.mode === "dark" ? "#101010" : "grey.100",
+					color: (theme) =>
+						theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+					border: "2px solid",
+					borderColor: (theme) =>
+						theme.palette.mode === "dark" ? "grey.800" : "grey.300",
 				}}
-				sx={modalStyle}
+				onSubmit={handleSubmit}
 			>
-				<Box
-					component="form"
-					sx={{
-						"& .MuiTextField-root": { m: 3, width: "25ch" },
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						bgcolor: (theme) =>
-							theme.palette.mode === "dark" ? "#101010" : "grey.100",
-						color: (theme) =>
-							theme.palette.mode === "dark" ? "grey.300" : "grey.800",
-						border: "2px solid",
-						borderColor: (theme) =>
-							theme.palette.mode === "dark" ? "grey.800" : "grey.300",
-					}}
-					onSubmit={handleSubmit}
+				<TextField
+					id="outlined"
+					select
+					label="Select"
+					helperText="Please select the category"
+					defaultValue={categories[0]}
+					variant="standard"
+					required
 				>
-					<TextField
-						id="outlined"
-						select
-						label="Select"
-						helperText="Please select the category"
-						defaultValue={categories[0]}
-						variant="standard"
-						required
-					>
-						{categories.map((category, index) => {
-							return (
-								<MenuItem key={index} value={category} onClick={handleCategory}>
-									{category}
-								</MenuItem>
-							);
-						})}
-					</TextField>
-					<TextField
-						id="standard-multiline-flexible"
-						placeholder="Please enter a brief description"
-						variant="standard"
-						required
-						onChange={handleDescription}
-					/>
-					<TextField
-						id="standard-number"
-						label="Amount"
-						type="number"
-						InputLabelProps={{
-							shrink: true,
-						}}
-						variant="standard"
-						required
-						onChange={handleAmount}
-					/>
-					<TextField
-						type="date"
-						defaultValue={new Date().toLocaleDateString()}
-						onChange={handleDate}
-					/>
-					<Button variant="contained">Add</Button>
-				</Box>
-			</Modal>
-		</div>
+					{categories.map((category, index) => {
+						return (
+							<MenuItem key={index} value={category} onClick={handleCategory}>
+								{category}
+							</MenuItem>
+						);
+					})}
+				</TextField>
+				<TextField
+					id="standard-multiline-flexible"
+					placeholder="Please enter a brief description"
+					variant="standard"
+					required
+					onChange={handleDescription}
+				/>
+				<TextField
+					id="standard-number"
+					label="Amount"
+					type="number"
+					InputLabelProps={{
+						shrink: true,
+					}}
+					variant="standard"
+					required
+					onChange={handleAmount}
+				/>
+				<TextField
+					type="date"
+					defaultValue={new Date().toLocaleDateString()}
+					onChange={handleDate}
+				/>
+				<Button variant="contained">Add</Button>
+			</Box>
+		</Modal>
 	);
 };
